08c30c17bc4fbd12c70bbc3ed70d8afbcb658d91
Markus Hofmann <markus.hofmann@schiller.de>
2022-09-05 17:41:29 +0200

    add multiple components showcasing behaviour

---
+++ BlazorApp1/Data/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Data
+{
+    public class DataItem
+    {
+        public DateTime Label { get; set; }
+        public decimal Data { get; set; }
+
+        public DataItem(DateTime label, decimal data)
+        {
+            Label = label;
+            Data = data;
+        }
+
+        public DataItem(DateTime label, int data) : this(label, Convert.ToDecimal(data)) { }
+    }
+}
---
+++ BlazorApp1/Pages/WorksButDelayed.razor.cs
@@ -0,0 +1,43 @@
+﻿using ApexCharts;
+using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorApp1.Pages
+{
+	public partial class WorksButDelayed
+	{
+        public List<(int id, List<DataItem> data)> Series = new();
+
+        private ApexChart<DataItem>? chart;
+
+        private int id = 0;
+
+        protected override void OnInitialized()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var data = new List<DataItem>();
+                for (int j = 0; j < 20; j++)
+                {
+                    data.Add(new DataItem(DateTime.UtcNow.AddDays(-20 + 1 + j).Date, Random.Shared.Next(10, 100)));
+                }
+                Series.Add(new(id++, data));
+            }
+        }
+
+        private async Task AddSeries(MouseEventArgs args)
+        {
+            var data = new List<DataItem>();
+            for (int j = 0; j < 20; j++)
+            {
+                data.Add(new DataItem(DateTime.UtcNow.AddDays(-20 + 1 + j).Date, Random.Shared.Next(10, 100)));
+            }
+            Series.Add(new(id++, data));
+
+            if (chart != null)
+            {
+                await chart.UpdateSeriesAsync();
+            }
+        }
+    }
+}
